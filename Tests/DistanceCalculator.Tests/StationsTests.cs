27b2c1c22fa274635af7d9a04986c3a0f7fcae13
Volodymyr Voloshyn <voloha13@ukr.net>
2017-12-24 20:01:46 +0200

    Stations unit tests

---
+++ Tests/DistanceCalculator.Tests/StationsTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using Stations;
+using DistanceCalculator.Tests.Stubs;
+
+namespace DistanceCalculator.Tests
+{
+	[TestFixture]
+	public class StationsTests
+	{
+		[Test]
+		public void Create_Station_Success()
+		{
+			// 
+			var stationId = StationData.StationId1;
+			var stationName = StationData.StationName1;
+			var stationLat = StationData.Station1Lat;
+			var stationLon = StationData.Station1Lon;
+
+			// Act
+			var station = new Station(stationId, stationName, stationLat, stationLon);
+
+			Assert.AreEqual(stationId, station.Id, "Station Id");
+			Assert.AreEqual(stationName, station.Name, "Station Name");
+			Assert.AreEqual(stationLat, station.Lat, "Station Lat");
+			Assert.AreEqual(stationLon, station.Lon, "Station Lon");
+		}
+	}
+}
---
+++ Tests/DistanceCalculator.Tests/Stubs/StationData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceCalculator.Tests.Stubs
+{
+	public static class StationData
+	{
+		public static int StationId1 { get { return 4321; } }
+		public static string StationName1 { get { return "Station1"; } }
+		public static double Station1Lat { get { return 34.6788; } }
+		public static double Station1Lon { get { return 22.2323; } }
+	}
+}
